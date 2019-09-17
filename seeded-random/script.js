89e41f01758d79a55d8f2e43522ca2988b119367
Artur Falborski <falborski.artur@gmail.com>
2019-09-17 11:58:25 +0200

    compare naive RNG approach with a widely-known one

--- seeded-random/script.js
+++ seeded-random/script.js
@@ -16,17 +16,31 @@ const getContext = (type, canvas) => canvas.getContext(type);
 
 const dataCanvas = attachCanvas({});
 
+const data2Canvas = attachCanvas({});
+
 const context = getContext('2d', dataCanvas);
 
-const customRand = num => {
+const context2 = getContext('2d', data2Canvas);
+
+// This is a mulberry32 implementation
+const customRand1 = a => {
+	let t = a += 0x6D2B79F5;
+	t = Math.imul(t ^ t >>> 15, t | 1);
+	t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+	return ((t ^ t >>> 14) >>> 0) / 4294967296;
+};
+
+
+// Naive approach
+const customRand2 = num => {
 	const x = Math.sin(num) * 10000;
 
 	return x - Math.floor(x);
 };
 
-const populateNewData = (items, fromSeed) => {
+const populateNewData = (items, fromSeed, fn) => {
 	console.time('populate');
-	let arr = new Array(items).fill(0).map((_, index) => customRand(fromSeed + index).toFixed(3));
+	let arr = new Array(items).fill(0).map((_, index) => fn(fromSeed + index).toFixed(3));
 
 	const data = arr.reduce((prev, curr) => {
 		if (prev[curr]) {
@@ -43,20 +57,23 @@ const populateNewData = (items, fromSeed) => {
 	return data
 };
 
-const draw = (data) => {
+const draw = (data, canvas, context) => {
 	console.time('draw');
 	context.fillStyle = '#000';
-	context.fillRect(0, 0, dataCanvas.width, dataCanvas.height);
+	context.fillRect(0, 0, canvas.width, canvas.height);
 	context.fillStyle = '#FF00FF';
 	for (let prop in data) {
-		context.fillRect(+prop * dataCanvas.width, dataCanvas.height, 1, -(data[prop] * 3))
+		context.fillRect(+prop * canvas.width, canvas.height, 1, -(data[prop] * 0.2))
 	}
 	console.timeEnd('draw');
 };
 
 
 const start = offset => {
-	draw(populateNewData(100000, offset * 100000))
+	const data1 = populateNewData(1000000, offset * 1000000, customRand1);
+	const data2 = populateNewData(1000000, offset * 1000000, customRand2);
+	draw(data1, dataCanvas, context);
+	draw(data2, data2Canvas, context2);
 
 	setTimeout(() => {
 		window.requestAnimationFrame(() => start(offset + 1));
